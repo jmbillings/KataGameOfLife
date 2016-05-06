651dcb376fed2ad6f9855423a6109f6176bc673c
jmbillings <james@puffle.co.uk>
2016-05-06 17:42:07 +0100

    Get game header to set row and column counts

---
+++ GameOfLifeKata/Game.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLifeKata
+{
+    public class Game
+    {
+        public int m_Columns;
+        public int m_Rows;
+
+        /// <summary>
+        /// Initialises a new game
+        /// </summary>
+        /// <param name="initialGameState">string representing the initial state of the game</param>
+        public Game(string initialGameState)
+        {
+            try
+            {
+                string[] headerRowValues = initialGameState.Substring(0, initialGameState.IndexOf('\n')).Split(' ');
+                if (!int.TryParse(headerRowValues[0], out m_Rows))
+                    throw new InvalidGameHeaderException();
+                if (!int.TryParse(headerRowValues[1], out m_Columns))
+                    throw new InvalidGameHeaderException();
+            }
+            catch
+            {
+                throw new InvalidGameHeaderException();
+            }
+        }
+
+        /// <summary>
+        /// Updates the game, calculating life and death
+        /// </summary>
+        public void UpdateGame()
+        {
+
+        }
+    }
+}
---
+++ GameOfLifeKata/InvalidGameHeaderException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameOfLifeKata
+{
+    class InvalidGameHeaderException : Exception
+    {
+        public override string ToString()
+        {
+            return "Invalid game state header found. Ensure the first row contains two numbers only";
+        }
+    }
+}
