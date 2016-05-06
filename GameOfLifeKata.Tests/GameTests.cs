4578b8aba89da1876763a357719a8aab1f361aa1
jmbillings <james@puffle.co.uk>
2016-05-06 17:42:18 +0100

    Add test for valid game header

--- GameOfLifeKata.Tests/GameTests.cs
+++ GameOfLifeKata.Tests/GameTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,17 @@ namespace GameOfLifeKata.Tests
 {
     public class GameTests
     {
+        [Test]
+        [TestCase("1 2\n", 1, 2)]
+        [TestCase("10 20\n", 10, 20)]
+        [TestCase("100 200\n", 100, 200)]
+        public void ValidGameHeaderSetsColumnsAndRowsCorrectly(string gameHeader, int expectedRows, int expectedColumns)
+        {
+            Game game = new Game(gameHeader);
+            Assert.AreEqual(game.m_Rows, expectedRows);
+            Assert.AreEqual(game.m_Columns, expectedColumns);    
+        }
+
+
     }
 }
