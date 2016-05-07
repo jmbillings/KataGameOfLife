da737c0e05895415f88ef04dc8e590c0ee7d297f
jmbillings <james@puffle.co.uk>
2016-05-07 12:31:59 +0100

    Add invalid character exception

---
+++ GameOfLifeKata/InvalidGameCharacterException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLifeKata
+{
+    public class InvalidGameCharacterException : Exception
+    {
+        public override string ToString()
+        {
+            return "Invalid character found in game definition";
+        }
+    }
+}
