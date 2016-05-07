2cce0e71925f1ae599e59523c1a0750c3cb00459
jmbillings <james@puffle.co.uk>
2016-05-07 11:48:44 +0100

    Red: Add test for initial game state check

--- GameOfLifeKata.Tests/GameTests.cs
+++ GameOfLifeKata.Tests/GameTests.cs
@@ -9,6 +9,13 @@ namespace GameOfLifeKata.Tests
 {
     public class GameTests
     {
+        private static object[] _sourceGames = {
+            new object[] {"1 1\n.", new bool[,] { { false } } },
+            new object[] {"1 1\n*", new bool[,] { { true } } },
+            new object[] {"2 2\n.*\n*.", new bool[,] { {false,true }, { true, false} } },
+            new object[] {"4 4\n...*\n..**\n....\n*...", new bool[,] { { false, false, false, true }, { false, false, true, true }, { false, false, false, false}, { true, false, false, false } } }
+                                    };
+
         [Test]
         [TestCase("1 2\n", 1, 2)]
         [TestCase("10 20\n", 10, 20)]
@@ -16,8 +23,8 @@ namespace GameOfLifeKata.Tests
         public void ValidGameHeaderSetsColumnsAndRowsCorrectly(string gameHeader, int expectedRows, int expectedColumns)
         {
             Game game = new Game(gameHeader);
-            Assert.AreEqual(game.m_RowCount, expectedRows);
-            Assert.AreEqual(game.m_ColumnCount, expectedColumns);    
+            Assert.AreEqual(expectedRows, game.m_RowCount);
+            Assert.AreEqual(expectedColumns, game.m_ColumnCount);
         }
 
         [Test]
@@ -32,6 +39,14 @@ namespace GameOfLifeKata.Tests
         {
             Game game;
             Assert.Throws<InvalidGameHeaderException>(() => game = new Game(gameHeader));
+        }
+
+        [Test]
+        [TestCaseSource("_sourceGames")]
+        public void InitialGameStateIsPopulatedCorrectly(string initialGameState, bool[,] expectedGameState)
+        {
+            Game game = new Game(initialGameState);
+            Assert.AreEqual(expectedGameState, game.m_Grid);
         }
     }
 }
